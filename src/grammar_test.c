48d187d17606b494fbbd6de2fff02584c92d54ed
soasme <soasme@gmail.com>
2023-02-16 21:55:01 +1300

    grammar: add program entry.

--- src/grammar_test.c
+++ src/grammar_test.c
@@ -3,19 +3,16 @@
 
 #define TEST_GRAMMAR(path, pass) {\
     ParserState state = {0}; \
-    AstNode* root = NULL; \
+    int ret = 0; \
     soc_context_t *parser = NULL; \
     if (setjmp(state.jmp) == 0) { \
         ParserState_Init(&state); \
         ParserState_Open(&state, (path)); \
         parser = soc_create(&state); \
-        const int b = soc_parse(parser, &root); \
-        ParserState_Dump(&state, root, stdin); \
-        if ((pass)) { \
-            TEST_ASSERT(b == 1); \
-        } else { \
-            TEST_ASSERT(b == 0); \
-        } \
+        const int b = soc_parse(parser, &ret); \
+        printf("ret = %d, b = %d", ret, b);\
+        TEST_ASSERT(ret == 0); \
+        TEST_ASSERT(b == 1); \
     } else {\
         TEST_ASSERT((pass) == 0); \
     }\
@@ -25,9 +22,7 @@
 
 int main(int argc, char **argv) {
     TEST_BEGIN(("grammar_test"));
-    TEST_GRAMMAR("src/tests/y_int_lit_100_000_000.txt", 1);
-    TEST_GRAMMAR("src/tests/y_hex_lit_0x8000_ff00.txt", 1);
-    TEST_GRAMMAR("src/tests/n_hex_lit_0xz.txt", 0);
+    TEST_GRAMMAR("src/tests/good.txt", 1);
     TEST_END();
     return 0;
 }
